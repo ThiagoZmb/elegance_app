6165b6a9650dee0f3fe6a8663c138600280eb867
Thiago <thiagofzamboni@gmail.com>
2025-07-11 17:05:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/login', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
