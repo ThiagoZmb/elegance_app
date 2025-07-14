65337a627fb85dc6d83fcc7d95a902014eae5415
Thiago <thiagofzamboni@gmail.com>
2025-07-14 18:43:49 -0300

    Update login.js

--- login.js
+++ login.js
@@ -132,7 +132,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
       
       // Redirecionar após 3 segundos
       setTimeout(() => {
-        window.location.href = "https://thiagozmb.github.io/elegance_app/painel_inicial.htm";
+        window.location.href = "https://thiagozmb.github.io/elegance_app/painel_inicial.html";
       }, 1000);
       
     } else {
