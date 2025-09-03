c32475cab431eda1b7092a9d8162c5d3c2ef64bf
Thiago <thiagofzamboni@gmail.com>
2025-09-03 14:00:22 -0300

    Update login.js

--- login.js
+++ login.js
@@ -36,7 +36,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     }
     
     const data = await res.json();
-    console.log('Resposta da API!!!!!!!!!:', data);
+    console.log('Resposta da API:', data);
     
     if (data.success) {
       // NOVO: Salvar dados do usuário no localStorage
